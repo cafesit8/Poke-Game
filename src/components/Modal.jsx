4d016b7cb83f00d5ba17095a62cdbd9786648d51
leliasgomer <leliasgomer@gmail.com>
2023-04-08 00:04:47 -0500

    se modificó el modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,9 +6,9 @@ export default function Modal({points, setModal, setPoints}) {
     }
 
   return (
-    <div className="absolute w-[500px] h-[500px] m-auto z-10 bg-blue-900 grid place-content-center">
-        <h3>Bien jugado, tu puntaje es de {points}</h3>
-        <button className="bg-gray-800 py-1 rounded-lg mt-2" onClick={handleClick}>Jugar de Nuevo</button>      
+    <div className="absolute w-[500px] p-4 h-[500px] rounded-xl m-auto z-10 bg-gray-800/90 grid place-content-center max-[530px]:w-[80%]">
+        <h3 className="text-xl text-center">Bien jugado, tu puntaje es de {points}</h3>
+        <button className="bg-gray-700 py-1 rounded-lg mt-3" onClick={handleClick}>Jugar de Nuevo</button>      
     </div>
   )
 }
